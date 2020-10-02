941501670750862743723e9207e0fb8a8eef015e
AugustoKlaic <gutosopelsa@hotmail.com>
2020-10-02 00:12:39 -0300

    removed CDI, will do the design pattern in java

--- IoC-CDI/src/main/java/testWithCDI/TestWithGuice.java
+++ IoC-CDI/src/main/java/testWithCDI/TestWithGuice.java
@@ -1,12 +1,8 @@
 package testWithCDI;
 
-import testWithCDI.beans.Store;
-
 public class TestWithGuice {
 
     public static void main(String[] args) {
 
-        Store store = new Store();
-        System.out.println(store);
     }
 }
